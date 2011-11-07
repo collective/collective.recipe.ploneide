57f5aa72c90fbcfb9bfca3a716f2a35f3c08503a
Mikko <mikko@mfabrik.com>
2011-11-07 03:49:39 +0200

    Reworked the start-up logic

--- collective/recipe/ploneide/__init__.py
+++ collective/recipe/ploneide/__init__.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
-"""Recipe ploneide"""
+"""
+
+    Recipe ploneide
+
+    Generate Plone debug + sauna.reload enabled launcher script in bin/
+
+"""
+import os.path
+
 import zc.buildout
 
 
@@ -10,17 +18,31 @@ class Recipe(object):
         self.egg = zc.recipe.egg.Egg(buildout, options['recipe'], options)
         self.buildout, self.name, self.options = buildout, name, options
         options['bin-directory'] = buildout['buildout']['bin-directory']
+
+        if not "instance" in buildout:
+            raise ValueError("Buildout missing [instance] section")
+
         options['eggs'] = buildout['instance']['eggs']
+
 
     def install(self):
         """Installer"""
         # XXX Implement recipe functionality here
         options = self.options
+
+        location = self.buildout["instance"]["location"]
+
+        zope_conf_path = os.path.join(location, "etc", "zope.conf")
+
         requirements, ws = self.egg.working_set(['collective.recipe.ploneide'])
         zc.buildout.easy_install.scripts(
             [(options.get('control-script', self.name),
               'collective.recipe.ploneide.debug', 'main')],
             ws, options['executable'], options['bin-directory'],
+            arguments = ('\n        ["-C", %r]'
+                         '\n        + sys.argv[1:]'
+                         % zope_conf_path
+                         ),
             )
 
         # Return files that were created by the recipe. The buildout
--- collective/recipe/ploneide/debug.py
+++ collective/recipe/ploneide/debug.py
@@ -1,8 +1,23 @@
+"""
+
+    Launch Zope w/debugger interface listening and sauna.reload running.
+
+"""
+import sys
+import os
 import os.path
 
 from collective.ploneide.ide_server import httpd
 from collective.ploneide.debug import debugger
 
+try:
+    # This will run in monkey patches giving in the fork reload
+    # support for plone
+    import sauna.reload
+    FROM_FINLAND_WITH_LOVE=True
+except ImportError:
+    print "No sauna.reload available"
+    FROM_FINLAND_WITH_LOVE=False
 
 def start(debugger, statement):
     # Let's start our auxiliar http server
@@ -14,8 +29,64 @@ def start(debugger, statement):
     # We are now quitting, let's shutdown our auxiliar server
     httpd.shutdown()
 
+def warm_up_the_sauna():
+    """
+    Configure in sauna.reload auto-reloading of SRC modules.
+    """
 
-def main(args=None):
-    idelpath = os.path.join(os.path.dirname(__file__), "idelauncher.py")
-    statement = ('execfile( "%s")' % (idelpath,))
-    start(debugger, statement)
+    print "Configuring module auto-reload"
+
+    # XXX: Do not assume getcwd() is buildout root, but get in from some opts
+
+    from sauna.reload.reloadpaths import ReloadPaths
+    sauna.reload.reload_paths = ReloadPaths([os.path.join(os.getcwd(), "src")])
+
+    from sauna.reload import autoinclude, fiveconfigure
+    from sauna.reload import reload_paths
+    from sauna.reload import monkeypatcher
+
+    monkeypatcher.PATCHED = True
+
+    if reload_paths:
+        # 1) Defer autoinclude of packages found under reload paths.
+        autoinclude.defer_paths()
+        # 2) Prevent Five from finding packages under reload paths.
+        fiveconfigure.defer_install()
+
+
+def run_zope(args):
+    """ Run Zope without a nested process launch.
+
+    This enables us to have the debugger intact for the process.
+
+    :param args:
+    """
+    # Start zope instance as a web server
+    # Zope launcher module
+    import Zope2.Startup.run
+
+    zope_main = Zope2.Startup.run.__file__
+
+    if zope_main.endswith(".pyc"):
+        # Run as .py file
+        zope_main = zope_main[:-1]
+
+    # Tinker with command-line to emulate normal Zope launch
+    sys.argv = [ zope_main, "-X", "debug-mode=on"] + args
+
+    # Instead of spawning zopectl and Zope in another process, execute Plone in the context of this Python interpreter
+    # to have pdb control over the code
+    #print "Executing: %s %s" % (zope_main, sys.argv)
+
+    if FROM_FINLAND_WITH_LOVE:
+        warm_up_the_sauna()
+
+    Zope2.Startup.run.run()
+
+import plone.recipe.zope2instance.ctl
+
+def main(args):
+    #idelpath = os.path.join(os.path.dirname(__file__), "idelauncher.py")
+    #statement = ('execfile( "%s")' % (idelpath,))
+    #start(debugger, statement)
+    run_zope(args)
